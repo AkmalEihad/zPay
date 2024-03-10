85c67f7ce776f9e7357b5e7ece857f63398fa6f5
yayad <eihad.rizalhanafi@gmail.com>
2024-03-10 12:31:47 +0800

    fix addAnimation

--- components/ui/infinite-moving-cards.tsx
+++ components/ui/infinite-moving-cards.tsx
@@ -1,14 +1,9 @@
 "use client";
 
 import {cn} from "@/lib/utils";
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 
-export const InfiniteMovingCards = ({
-                                        items,
-                                        direction = "left",
-                                        speed = "fast",
-                                        pauseOnHover = true,
-                                        className,
+export const InfiniteMovingCards = ({items, direction = "left", speed = "fast", pauseOnHover = false, className,
                                     }: {
     items: {
         quote: string;
@@ -23,8 +18,10 @@ export const InfiniteMovingCards = ({
     const containerRef = React.useRef<HTMLDivElement>(null);
     const scrollerRef = React.useRef<HTMLUListElement>(null);
 
+    const addAnimationRef = useRef(addAnimation);
+
     useEffect(() => {
-        addAnimation();
+        addAnimationRef.current();
     }, []);
     const [start, setStart] = useState(false);
 
@@ -87,7 +84,7 @@ export const InfiniteMovingCards = ({
                     pauseOnHover && "hover:[animation-play-state:paused]"
                 )}
             >
-                {items.map((item, idx) => (
+                {items.map((item) => (
                     <li
                         className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
                         style={{
